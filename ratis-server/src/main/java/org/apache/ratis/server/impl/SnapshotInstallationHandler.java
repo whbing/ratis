4c92b4cd6e5d863d70b22d638a58c963ae6a3522
wanghongbing <wanghongbing@didiglobal.com>
2024-04-20 22:02:00 +0800

    Revert "RATIS-2045. SnapshotInstallationHandler doesn't notify follower when snapshotIndex is -1 and firstAvailableLogIndex is 0 (#1053)"
    This reverts commit 72c060d9cc524045ce55e830db070c5514f23ed4.

--- ratis-server/src/main/java/org/apache/ratis/server/impl/SnapshotInstallationHandler.java
+++ ratis-server/src/main/java/org/apache/ratis/server/impl/SnapshotInstallationHandler.java
@@ -221,8 +221,7 @@ class SnapshotInstallationHandler {
         // Check if snapshot index is already at par or ahead of the first
         // available log index of the Leader.
         final long snapshotIndex = state.getLog().getSnapshotIndex();
-        if (snapshotIndex != INVALID_LOG_INDEX && snapshotIndex + 1 >= firstAvailableLogIndex &&
-            firstAvailableLogIndex > INVALID_LOG_INDEX) {
+        if (snapshotIndex + 1 >= firstAvailableLogIndex && firstAvailableLogIndex > INVALID_LOG_INDEX) {
           // State Machine has already installed the snapshot. Return the
           // latest snapshot index to the Leader.
 
